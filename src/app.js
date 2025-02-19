d02bb9a92123c6d45d25d5e59e31f9b0ceed0b4e
IsNoobDoog <50238909+IsNoobDoog@users.noreply.github.com>
2025-02-19 20:17:29 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
-import "bootstrap";
-import "./style.css";
-
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+/* import "bootstrap";
+import "./style.css"
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+    return;
 };
+ */
+const generarDominio = () => {
+
+    let pronoun = ['the', 'our'];
+    let adj = ['great', 'big'];
+    let noun = ['jogger', 'racoon'];
+    let com = ['.com', '.es', '.ar']
+    //write your code here
+    for (let i of pronoun)
+    for (let j of adj)
+    for (let n of noun)
+    for (let c of com)
+    console.log(i + j + n + c);
+}
+    generarDominio()
